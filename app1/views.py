0bf67643f3ae5e4a3b40c84b69f31bbdbb1e2270
kaushito <kaushoficial@gmail.com>
2023-09-25 08:04:57 -0300

    app1 rterminada

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def vistaUno(request):
+    html= """
+    <h1 style="color:blue">Hola Mundo</h1>
+ """
+    return HttpResponse(html)
